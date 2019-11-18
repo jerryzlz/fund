ed966fc0199fa323d224bd32ad5a90bbbddc84a6
jerryzlz <jerryzlz4@hotmail.com>
2019-11-18 22:53:40 +0800

    Update fund.py

--- fund.py
+++ fund.py
@@ -1,9 +1,9 @@
-# Version: 1.1.1
+# Version: 1.1.2
 # Author: 郑力中
 # Without GUI
 
 
-
+import time
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,7 +16,7 @@ else:
     url = 'http://fund.eastmoney.com/' + num + '.html'
     res = requests.get(url)
     res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text,features="lxml")
+    soup = BeautifulSoup(res.text,features="html.parser")
     pre_QDII = soup.select('.infoOfFund a')
     QDII = pre_QDII[0].text
     pre_guzhi = soup.select('#gz_gszzl')
@@ -24,23 +24,48 @@ else:
     pre_name = soup.select('.fundDetail-tit')
     name = pre_name[0].text
 
-pre_sum = soup.select('.RelatedInfo')
-g = [str(pre_sum[10].text),str(pre_sum[11].text),str(pre_sum[12].text),str(pre_sum[13].text),str(pre_sum[14].text),
-    str(pre_sum[15].text),str(pre_sum[16].text),str(pre_sum[17].text),str(pre_sum[18].text),str(pre_sum[19].text)]
-cou = [g[0].count('-'),g[1].count('-'),g[2].count('-'),g[3].count('-'),g[4].count('-'),
-    g[5].count('-'),g[6].count('-'),g[7].count('-'),g[8].count('-'),g[9].count('-')]
-final_count = cou[0] + cou[1] + cou [2] + cou [3] + cou[4] + cou[5] + cou[6] + cou[7] + cou [8] + cou [9]
-if guzhi[0] == '+':
-        print ('今日' + name + '估值为涨，预计涨幅'+ guzhi[1:6] + '，不需要任何操作')
+if QDII == 'QDII-指数':
+    url_QDII = "http://jingzhi.funds.hexun.com/DataBase/jzzs.aspx?fundcode=" + num
+    res_QDII = requests.get(url_QDII)
+    res_QDII.encoding = 'utf-8'
+    soup = BeautifulSoup(res_QDII.text,features="html.parser")
+    pre_QDII_jingzhi = soup.select('#')
+    print(pre_QDII_jingzhi)
 else:
-        print('今日' + name + '估值为跌，预计跌幅'+ guzhi[1:6] + '，可以进行适当加仓')
-
-print ('')
-print ('长期建议：')
-print ('近10个交易日下跌' + str(final_count) + '次，建议：')
-if final_count >= 3:
-    print ("加仓")
-else:
-    print ('过高不买入')
-print ('')
-print ('！！！以上结果仅供参考！！！')
+    pre_sum = soup.select('.RelatedInfo')
+    sum_g = 10
+    g = []
+    while sum_g < 20:
+        g.append(pre_sum[sum_g].text)
+        sum_g = sum_g + 1
+    sum_cou = 0
+    final_count = 0
+    cou = []
+    while sum_cou < 10:
+        cou.append(g[sum_cou].count('-'))
+        final_count = final_count + cou[sum_cou]
+        sum_cou = sum_cou + 1
+    nowtime = time.strftime('%H:%M:%S',time.localtime(time.time()))
+    print ('短期建议：')
+    if nowtime[0:2] <= '14':
+        if guzhi[0] == '+':
+            print ('今日' + name + '估值为涨，预计涨幅'+ guzhi[1:6] + '，不需要任何操作。')
+            print ('现在时间为' + nowtime + '还未即将收盘，建议等待14点后再运行决策系统。')
+        else:
+            print('今日' + name + '估值为跌，预计跌幅'+ guzhi[1:6] + '，可以进行适当加仓。')
+            print('现在时间为' + nowtime + '还未即将收盘，建议等待14点后再运行决策系统。')
+    else :
+        if guzhi[0] == '+':
+            print ('今日' + name + '估值为涨，预计涨幅'+ guzhi[1:6] + '，不需要任何操作。')
+        else:
+            print('今日' + name + '估值为跌，预计跌幅'+ guzhi[1:6] + '，可以进行适当加仓。')
+    
+    print ('')
+    print ('长期建议：')
+    print ('近10个交易日下跌' + str(final_count) + '次，建议：')
+    if final_count >= 3:
+        print ('加仓')
+    else:
+        print ('过高不买入')
+    print ('')
+    print ('！！！以上结果仅供参考！！！')
